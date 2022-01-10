71687eaa3c5bfc01037e00914c03dbf918adc735
Jan Heinrich Reimer <heinrich@reimer.family>
2022-01-11 00:32:43 +0100

    Improve linting

--- ir_axioms/axiom/all.py
+++ ir_axioms/axiom/all.py
@@ -1,9 +1,9 @@
 # Expose axiom implementations.
-import ir_axioms.axiom.argumentative  # noqa
-import ir_axioms.axiom.length_norm  # noqa
-import ir_axioms.axiom.lower_bound  # noqa
-import ir_axioms.axiom.proximity  # noqa
-import ir_axioms.axiom.query_aspects  # noqa
-import ir_axioms.axiom.retrieval_score  # noqa
-import ir_axioms.axiom.term_frequency  # noqa
-import ir_axioms.axiom.term_similarity  # noqa
+import ir_axioms.axiom.argumentative  # noqa: F401
+import ir_axioms.axiom.length_norm  # noqa: F401
+import ir_axioms.axiom.lower_bound  # noqa: F401
+import ir_axioms.axiom.proximity  # noqa: F401
+import ir_axioms.axiom.query_aspects  # noqa: F401
+import ir_axioms.axiom.retrieval_score  # noqa: F401
+import ir_axioms.axiom.term_frequency  # noqa: F401
+import ir_axioms.axiom.term_similarity  # noqa: F401
