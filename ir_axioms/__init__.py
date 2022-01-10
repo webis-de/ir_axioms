0370aae6d53373139acb9372463b7849f905786c
Jan Heinrich Reimer <heinrich@reimer.family>
2022-01-10 12:59:11 +0100

    Add logger

--- ir_axioms/__init__.py
+++ ir_axioms/__init__.py
@@ -1,7 +1,10 @@
+from logging import basicConfig, Logger, getLogger
 from typing import List
 
 __version__ = "0.1.0"
 
+basicConfig()
+logger: Logger = getLogger(__name__)
 
 def main(args: List[str]):
     raise NotImplementedError()
