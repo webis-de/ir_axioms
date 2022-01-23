9e27c6972b44fef9a84e08173bb100e9bdd29a5c
Jan Heinrich Reimer <heinrich@reimer.family>
2022-01-23 16:05:15 +0100

    Add uniform axiom

--- ir_axioms/axiom/arithmetic.py
+++ ir_axioms/axiom/arithmetic.py
@@ -7,6 +7,20 @@ from ir_axioms.model.context import RerankingContext
 
 
 @dataclass(frozen=True)
+class UniformAxiom(Axiom):
+    preference: float
+
+    def preference(
+            self,
+            context: RerankingContext,
+            query: Query,
+            document1: RankedDocument,
+            document2: RankedDocument
+    ) -> float:
+        return self.preference
+
+
+@dataclass(frozen=True)
 class SumAxiom(Axiom):
     axioms: Iterable[Axiom]
 
@@ -21,6 +35,7 @@ class SumAxiom(Axiom):
             axiom.preference(context, query, document1, document2)
             for axiom in self.axioms
         )
+
 
 @dataclass(frozen=True)
 class WeightedAxiom(Axiom):
