bd5c8c99db22dbe9b1d64598f30f97f47bb1887a
Jan Heinrich Reimer <heinrich@reimer.family>
2022-01-24 02:19:37 +0100

    Add aggregation parameter for axiom conversion

--- ir_axioms/axiom/conversion.py
+++ ir_axioms/axiom/conversion.py
@@ -1,19 +1,31 @@
-from typing import Iterable, Union
+from typing import Iterable, Union, Type, TypeVar
 
 from ir_axioms import registry
-from ir_axioms.axiom.arithmetic import SumAxiom
+from ir_axioms.axiom.arithmetic import SumAxiom, ProductAxiom, \
+    MajorityVoteAxiom, AndAxiom
 from ir_axioms.axiom.base import Axiom
 from ir_axioms.model import Query, RankedDocument
 from ir_axioms.model.context import RerankingContext
 
 AxiomLike = Union[str, Axiom, Iterable["AxiomLike"]]
 
+AggregationAxiom = TypeVar(
+    "AggregationAxiom",
+    SumAxiom,
+    ProductAxiom,
+    AndAxiom,
+    MajorityVoteAxiom,
+)
 
-def to_axiom(axiom_like: AxiomLike) -> Axiom:
+
+def to_axiom(
+        axiom_like: AxiomLike,
+        aggregation: Type[AggregationAxiom] = SumAxiom
+) -> Axiom:
     if isinstance(axiom_like, str):
         return registry[axiom_like]
     elif isinstance(axiom_like, Iterable):
-        return SumAxiom([to_axiom(item) for item in axiom_like])
+        return aggregation([to_axiom(item) for item in axiom_like])
     else:
         assert isinstance(axiom_like, Axiom)
         return axiom_like
@@ -22,8 +34,12 @@ def to_axiom(axiom_like: AxiomLike) -> Axiom:
 class AutoAxiom(Axiom):
     _axiom: Axiom
 
-    def __init__(self, axiom_like: AxiomLike):
-        self._axiom = to_axiom(axiom_like)
+    def __init__(
+            self,
+            axiom_like: AxiomLike,
+            aggregation: Type[AggregationAxiom] = SumAxiom
+    ):
+        self._axiom = to_axiom(axiom_like, aggregation)
 
     def preference(
             self,
