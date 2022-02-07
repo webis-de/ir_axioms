7fe4ce0ebe26515d8ad667e757bfa143557745ab
Jan Heinrich Reimer <heinrich@reimer.family>
2022-02-08 00:58:11 +0100

    Simplify axiom cache

--- ir_axioms/axiom/cache.py
+++ ir_axioms/axiom/cache.py
@@ -1,6 +1,7 @@
-from collections import OrderedDict as OrderedDictImpl
-from dataclasses import field, dataclass
-from typing import OrderedDict, Tuple
+from dataclasses import dataclass
+from typing import Tuple, Optional
+
+from diskcache import Cache
 
 from ir_axioms.axiom.base import Axiom
 from ir_axioms.model import RankedDocument, Query
@@ -8,85 +9,22 @@ from ir_axioms.model.context import RerankingContext
 
 
 @dataclass(frozen=True)
-class _AxiomLRUCache:
-    capacity: int = 4096
-
-    # In the cache, the document pairs are stored
-    # in ascending hash value order in order to benefit
-    # from axioms' symmetric.
-    _cache: OrderedDict[
-        Tuple[RerankingContext, Query, RankedDocument, RankedDocument],
-        float
-    ] = field(
-        default_factory=lambda: OrderedDictImpl(),
-        init=False,
-        repr=False
-    )
-
-    @staticmethod
-    def _sign(
-            document1: RankedDocument,
-            document2: RankedDocument
-    ) -> int:
-        if hash(document1) <= hash(document2):
-            return 1
-        else:
-            return -1
-
-    def __contains__(
-            self,
-            key: Tuple[
-                RerankingContext,
-                Query,
-                RankedDocument,
-                RankedDocument
-            ],
-    ):
-        return key in self._cache
-
-    def __getitem__(
-            self,
-            key: Tuple[
-                RerankingContext,
-                Query,
-                RankedDocument,
-                RankedDocument
-            ],
-    ):
-        (_, _, document1, document2) = key
-        sign = self._sign(document1, document2)
-
-        value = self._cache[key] * sign
-        self._cache.move_to_end(key)
-        return value
-
-    def __setitem__(
-            self,
-            key: Tuple[
-                RerankingContext,
-                Query,
-                RankedDocument,
-                RankedDocument
-            ],
-            preference: float,
-    ):
-        (_, _, document1, document2) = key
-        sign = self._sign(document1, document2)
-
-        value = preference * sign
-        self._cache[key] = value
-        self._cache.move_to_end(key)
-
-        if len(self._cache) > self.capacity:
-            self._cache.popitem(last=False)
-
-
-@dataclass(frozen=True)
 class CachedAxiom(Axiom):
     axiom: Axiom
-    capacity: int = 4096
 
-    _cache = _AxiomLRUCache(capacity)
+    @staticmethod
+    def _cache(context: RerankingContext) -> Optional[Cache]:
+        if context.cache_dir is None:
+            return None
+        return Cache(str(context.cache_dir.absolute()))
+
+    @staticmethod
+    def _key(
+            query: Query,
+            document1: RankedDocument,
+            document2: RankedDocument
+    ) -> Tuple[str, str, str]:
+        return query.title, document1.id, document2.id
 
     def preference(
             self,
@@ -95,22 +33,22 @@ class CachedAxiom(Axiom):
             document1: RankedDocument,
             document2: RankedDocument
     ) -> float:
-        if (context, query, document1, document2) in self._cache:
-            return self._cache[(context, query, document1, document2)]
-        else:
-            preference = self.axiom.preference(
-                context,
-                query,
-                document1,
-                document2
-            )
-            self._cache[(context, query, document1, document2)] = preference
+        cache = self._cache(context)
+
+        if cache is None:
+            return self.axiom.preference(context, query, document1, document2)
+
+        key = self._key(query, document1, document2)
+
+        if key in cache:
+            return cache[key]
+
+        inverse_key = self._key(query, document2, document1)
+        if inverse_key in cache:
+            inverse_preference = cache[inverse_key]
+            preference = -inverse_preference
+            cache[key] = preference
             return preference
 
-    def cached(self, capacity: int = 4096) -> Axiom:
-        if self.capacity >= capacity:
-            # This axiom is already cached with a larger.
-            return self
-        else:
-            # Cache wrapped axiom with higher capacity.
-            return CachedAxiom(self.axiom, capacity)
+    def cached(self) -> Axiom:
+        return self
