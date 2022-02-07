f5f75a54be7990b7c3b879aa55905c16fff27158
Maik Fröbe <maik.froebe@informatik.uni-halle.de>
2022-02-07 21:02:50 +0100

    script to start notebook on gammaweb

--- start-notebook-on-gammaweb.sh
+++ start-notebook-on-gammaweb.sh
@@ -5,5 +5,6 @@ srun-capreolus-notebook:
 		--container-writable \
 		--container-image=pytorch/pytorch:1.8.0-cuda11.1-cudnn8-devel \
 		--container-name=sigir22-ir-axioms --pty \
+		--container-mounts=${PWD}:/workspace,/mnt/ceph/storage/data-tmp/2021/kibi9872/.ir_datasets:/root/.ir_datasets \
 		--chdir ${PWD} \
 		bash -c 'cd ~/ecir22-zero-shot/ && jupyter-lab --ip 0.0.0.0 --allow-root'
