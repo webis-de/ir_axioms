45d3ed0ddff1ef9b28888afea806e4aac916b47f
Jan Heinrich Reimer <heinrich@reimer.family>
2022-02-08 17:51:02 +0100

    Auto-install dependencies in Slurm

--- slurm-start-jupyter-lab.sh
+++ slurm-start-jupyter-lab.sh
@@ -1,12 +1,13 @@
 #!/bin/bash -e
 
+[ -d "$HOME"/.ir_datasets ] || mkdir "$HOME"/.ir_datasets
 srun \
   --cpus-per-task 1 \
   --mem=100G \
-	--container-writable \
-	--container-image=python:3.8.10 \
-	--container-name=sigir22-ir-axioms-02 \
-	--pty \
-	--container-mounts="$PWD":/workspace,~/.ir_datasets:/root/.ir_datasets \
-	--chdir "$PWD" \
-	bash -c "cd /workspace && pip install .[pyterrier,test] && jupyter-lab --ip 0.0.0.0 --allow-root"
+  --container-writable \
+  --container-image=python:3.8.10 \
+  --container-name=sigir22-ir-axioms-02 \
+  --pty \
+  --container-mounts="$PWD":/workspace,"$HOME"/.ir_datasets:/root/.ir_datasets \
+  --chdir "$PWD" \
+  bash -c "cd /workspace && python -m pip install --upgrade pip && python -m pip install -e . -e .[pyterrier] -e .[test] && jupyter-lab --ip 0.0.0.0 --allow-root"
