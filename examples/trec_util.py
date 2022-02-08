7778fcec35429bf2bf903fa89432e62d666f0535
Jan Heinrich Reimer <heinrich@reimer.family>
2022-02-08 01:23:36 +0100

    Speed up result loading

--- examples/trec_util.py
+++ examples/trec_util.py
@@ -1,7 +1,7 @@
 from dataclasses import dataclass
 from functools import cached_property
 from pathlib import Path
-from typing import List, Any
+from typing import List
 
 from ir_datasets import load
 from pandas import DataFrame
@@ -9,7 +9,7 @@ from pyterrier import Transformer
 from pyterrier.datasets import IRDSDataset, get_dataset, Dataset
 from pyterrier.index import IterDictIndexer
 from pyterrier.io import read_results
-from pyterrier.transformer import TransformerBase, SourceTransformer
+from pyterrier.transformer import TransformerBase
 from tqdm import tqdm
 
 
@@ -18,6 +18,7 @@ class TrecTrack:
     edition: int
     track: str
     dataset_name: str
+    cache_dir: Path
     base_results_dir: Path = Path(
         "/mnt/ceph/storage/data-in-progress/data-research/"
         "web-search/web-search-trec/trec-system-runs"
@@ -34,7 +35,7 @@ class TrecTrack:
     @cached_property
     def index(self) -> Path:
         # Load documents and build index.
-        index_dir = Path(f"./data/indices/{self.dataset_name}")
+        index_dir = self.cache_dir / "indices" / self.dataset_name
         if not index_dir.exists():
             # Don't forget to include the 'text' field in the meta index.
             indexer = IterDictIndexer(str(index_dir))
@@ -59,21 +60,34 @@ class TrecTrack:
             total=num_files,
         )
         return [
-            ~(
-                    SourceTransformer(
-                        read_results(
-                            str(path.absolute()),
-                            dataset=dataset,
-                        )
-                    ) >> TaggedTransformer(str(path.absolute()))
+            _TrecTrackSourceTransformer(
+                str(path.absolute()),
+                dataset,
             )
             for path in files
         ]
 
 
 @dataclass(frozen=True)
-class TaggedTransformer(TransformerBase):
-    tag: Any
+class _TrecTrackSourceTransformer(TransformerBase):
+    filename: str
+    dataset: IRDSDataset
 
-    def transform(self, ranking: DataFrame) -> DataFrame:
-        return ranking
+    @cached_property
+    def _results(self) -> DataFrame:
+        return read_results(
+            self.filename,
+            dataset=self.dataset,
+        )
+
+    def transform(self, topics):
+        assert "qid" in topics.columns
+        results = self._results
+
+        columns = ["qid"]
+        results_contain_query = "query" in results.columns
+        topics_contains_query = "query" in topics.columns
+        if not results_contain_query and topics_contains_query:
+            columns.append("query")
+        retrieved = topics[columns].merge(results, on="qid")
+        return retrieved
