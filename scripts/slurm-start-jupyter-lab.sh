d9db762f60de047dd8293de9d94a0f28717fc9b9
Jan Heinrich Reimer <heinrich@reimer.family>
2022-02-10 05:19:38 +0100

    Fix Slurm script

--- scripts/slurm-start-jupyter-lab.sh
+++ scripts/slurm-start-jupyter-lab.sh
@@ -5,18 +5,15 @@ srun \
   --cpus-per-task 1 \
   --mem=100G \
   --container-writable \
-  --container-image=alpine:3.15 \
-  --container-name=sigir22-ir-axioms-"$USER" \
-  --pty \
+  --container-image=python:3.9-buster \
+  --container-name=sigir22-ir-axioms-"$USER"-python3.9-jdk11 \
   --container-mounts="$PWD":/workspace,"$HOME"/.ir_datasets:/root/.ir_datasets \
   --chdir "$PWD" \
-  bash -c "cd /workspace &&
-    apk update &&
-    apk upgrade &&
-    apk add bash unzip curl openjdk7-jre python3
-    python3 -m ensurepip &&
-    pip3 install --upgrade pip setuptools &&
-    pip3 install -e . &&
-    pip3 install -e .[pyterrier] &&
-    pip3 install -e .[test] &&
+  --pty \
+  su -c "cd /workspace &&
+    apt update &&
+    apt install openjdk-11-jdk &&
+    export JAVA_HOME=/usr/lib/jvm/java-11-openjdk-amd64/ &&
+    pip install cython &&
+    pip install --editable .[test,pyterrier] &&
     jupyter-lab --ip 0.0.0.0 --allow-root"
