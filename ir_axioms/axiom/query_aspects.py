d9b22be561ebcec6b0772724e84144c07cb5cfd9
Jan Heinrich Reimer <heinrich@reimer.family>
2022-02-11 16:43:13 +0100

    Add ASPECT-REG axiom

--- ir_axioms/axiom/query_aspects.py
+++ ir_axioms/axiom/query_aspects.py
@@ -1,11 +1,13 @@
 from abc import ABC
 from dataclasses import dataclass
+from itertools import combinations
+from statistics import mean
 from typing import Set
 
 from ir_axioms import logger
 from ir_axioms.axiom.base import Axiom
 from ir_axioms.axiom.preconditions import LEN_Mixin
-from ir_axioms.axiom.utils import strictly_greater
+from ir_axioms.axiom.utils import strictly_greater, approximately_equal
 from ir_axioms.model import Query, RankedDocument, IndexContext
 from ir_axioms.utils.similarity import (
     TermSimilarityMixin, WordNetSynonymSetTermSimilarityMixin,
@@ -143,6 +145,91 @@ class ANTI_REG_fastText(_ANTI_REG, FastTextWikiNewsTermSimilarityMixin):
 
 
 @dataclass(frozen=True)
+class _ASPECT_REG(Axiom, TermSimilarityMixin, ABC):
+    """
+    Similar to REG but follows the query aspect clustering
+    from the paper and then counts the number of aspects covered
+    in each document.
+
+    Reference:
+    Zheng, W., Fang, H.: Query aspect based term weighting regularization
+    in information retrieval. In: Gurrin, C., et al. (eds.) ECIR 2010.
+    """
+
+    def preference(
+            self,
+            context: IndexContext,
+            query: Query,
+            document1: RankedDocument,
+            document2: RankedDocument
+    ):
+        query_terms = context.term_set(query)
+        document1_terms = context.term_set(document1)
+        document2_terms = context.term_set(document2)
+        if len(query_terms) == 0:
+            return 0
+
+        term_discriminators = {
+            context.inverse_document_frequency(term)
+            for term in query_terms
+        }
+        if not approximately_equal(*term_discriminators):
+            # Require same term discriminator for all query terms.
+            return 0
+
+        average_similarity = mean(
+            self.similarity(term1, term2)
+            for term1, term2 in combinations(query_terms, 2)
+        )
+
+        query_aspects: Set[Set[str]] = {{term} for term in query_terms}
+        while len(query_aspects) > 1:
+            for a1, a2 in combinations(query_aspects, 2):
+                a1: Set[str]
+                a2: Set[str]
+                if any(
+                        self.similarity(term1, term2) > average_similarity
+                        for term1 in a1
+                        for term2 in a2
+                ):
+                    # Remove mergable aspects.
+                    query_aspects = {
+                        aspect
+                        for i, aspect in enumerate(query_aspects)
+                        if aspect != a1 and aspect != a2
+                    }
+                    # Merge aspects.
+                    merged_aspect = a1 | a2
+                    query_aspects.add(merged_aspect)
+
+            # No more mergable aspects.
+            break
+
+        count_document1_aspects = {
+            1 for aspect in query_aspects
+            if not document1_terms.isdisjoint(aspect)
+        }
+        count_document2_aspects = {
+            1 for aspect in query_aspects
+            if not document2_terms.isdisjoint(aspect)
+        }
+        return strictly_greater(
+            count_document1_aspects,
+            count_document2_aspects
+        )
+
+
+@dataclass(frozen=True)
+class ASPECT_REG(_ASPECT_REG, WordNetSynonymSetTermSimilarityMixin):
+    name = "C-REG"
+
+
+@dataclass(frozen=True)
+class ASPECT_REG_fastText(_ASPECT_REG, FastTextWikiNewsTermSimilarityMixin):
+    name = "C-REG-fastText"
+
+
+@dataclass(frozen=True)
 class AND(Axiom):
     name = "AND"
 
@@ -226,3 +313,4 @@ class LEN_DIV(LEN_Mixin, DIV):
 # Shorthand names:
 REG_f = REG_fastText
 ANTI_REG_f = ANTI_REG_fastText
+ASPECT_REG_f = ASPECT_REG_fastText
