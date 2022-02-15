98f4cce57965df6b0b93034eb46ec7fc74ea9674
Jan Heinrich Reimer <heinrich@reimer.family>
2022-02-15 23:53:13 +0100

    Add more tests for empty content

--- tests/unit/test_empty_content.py
+++ tests/unit/test_empty_content.py
@@ -58,6 +58,30 @@ def test_empty_query(axiom: Axiom):
     assert axiom.preference(context, query, document2, document1) == 0
 
 
+def test_empty_document1(axiom: Axiom):
+    query = Query("w1 w2 w3")
+    document1 = RankedTextDocument("d1", 2, 1, "")
+    document2 = RankedTextDocument("d2", 1, 2, "w1 w2 w3")
+    context = MemoryIndexContext({document1, document2})
+
+    assert (
+            axiom.preference(context, query, document1, document2) ==
+            -axiom.preference(context, query, document2, document1)
+    )
+
+
+def test_empty_document2(axiom: Axiom):
+    query = Query("w1 w2 w3")
+    document1 = RankedTextDocument("d1", 2, 1, "w1 w2 w3")
+    document2 = RankedTextDocument("d2", 1, 2, "")
+    context = MemoryIndexContext({document1, document2})
+
+    assert (
+            axiom.preference(context, query, document1, document2) ==
+            -axiom.preference(context, query, document2, document1)
+    )
+
+
 def test_empty_documents(axiom: Axiom):
     query = Query("w1 w2 w3")
     document1 = RankedTextDocument("d1", 2, 1, "")
