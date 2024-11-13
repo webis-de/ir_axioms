9a6e99dc9f703792941eaadada4a25a062510c51
Jan Heinrich Merker <heinrich.merker@uni-jena.de>
2024-11-13 11:17:39 +0100

    Fix test

--- tests/unit/test_string_representations.py
+++ tests/unit/test_string_representations.py
@@ -4,14 +4,14 @@ from axioms.backend.pyterrier import TerrierIndexContext
 def test_string_representation_of_terrier_index_context_01():
     # Needed for caching
     expected = "TerrierIndexContext(index_location)"
-    actual = str(TerrierIndexContext("index_location"))
+    actual = str(TerrierIndexContext(index_location="index_location"))
     assert expected == actual
 
 
 def test_string_representation_of_terrier_index_context_02():
     # Needed for caching
     expected = "TerrierIndexContext(index_location)"
-    actual = str(TerrierIndexContext("ignore/absolute/path/index_location"))
+    actual = str(TerrierIndexContext(index_location="ignore/absolute/path/index_location"))
     assert expected == actual
 
 
@@ -19,6 +19,6 @@ def test_string_representation_of_terrier_index_context_03():
     # Needed for caching
     expected = "TerrierIndexContext(index_location)"
     actual = str(
-        TerrierIndexContext("ignore/absolute/path/index_location ignore suffix")
+        TerrierIndexContext(index_location="ignore/absolute/path/index_location ignore suffix")
     )
     assert expected == actual
