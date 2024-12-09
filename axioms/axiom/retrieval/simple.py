b073a06ae5eaf96bdd3ee18b7eaaccdfd5152722
Jan Heinrich Merker <heinrich.merker@uni-jena.de>
2024-12-09 22:35:12 +0100

    Refactor term frequency axioms to use injected tools

--- axioms/axiom/retrieval/simple.py
+++ axioms/axiom/retrieval/simple.py
@@ -1,34 +1,50 @@
 from dataclasses import dataclass
+from typing import Any, TypeVar
 
 from axioms.axiom.base import Axiom
 from axioms.axiom.utils import strictly_less, strictly_greater
-from axioms.model import Query, RankedDocument, JudgedDocument
+from axioms.dependency_injection import injector
+from axioms.model import ScoredDocument, RankedDocument, JudgedDocument
+from axioms.utils.lazy import lazy_inject
+
+
+_RankedOrScoredDocument = TypeVar(
+    "_RankedOrScoredDocument", RankedDocument, ScoredDocument
+)
 
 
 @dataclass(frozen=True, kw_only=True)
-class OriginalAxiom(Axiom[Query, RankedDocument]):
+class OriginalAxiom(Axiom[Any, _RankedOrScoredDocument]):
 
     def preference(
         self,
-        input: Query,
-        output1: RankedDocument,
-        output2: RankedDocument,
-    ):
-        return strictly_less(output1.rank, output2.rank)
+        input: Any,
+        output1: _RankedOrScoredDocument,
+        output2: _RankedOrScoredDocument,
+    ) -> float:
+        if isinstance(output1, ScoredDocument) and isinstance(output2, ScoredDocument):
+            return strictly_greater(output1.score, output2.score)
+        elif isinstance(output1, RankedDocument) and isinstance(
+            output2, RankedDocument
+        ):
+            return strictly_less(output1.rank, output2.rank)
+        else:
+            raise ValueError("Can only compare RankedDocument's or ScoredDocument's.")
+
+
+ORIG = lazy_inject(OriginalAxiom, injector)
 
 
 @dataclass(frozen=True, kw_only=True)
-class OracleAxiom(Axiom[Query, JudgedDocument]):
+class OracleAxiom(Axiom[Any, JudgedDocument]):
 
     def preference(
         self,
-        input: Query,
+        input: Any,
         output1: JudgedDocument,
         output2: JudgedDocument,
     ) -> float:
         return strictly_greater(output1.relevance, output2.relevance)
 
 
-# Aliases for shorter names:
-ORIG = OriginalAxiom
-ORACLE = OracleAxiom
+ORACLE = lazy_inject(OracleAxiom, injector)
