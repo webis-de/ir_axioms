b7c6c0116ea873cb0a10d282b4887b3496233ccd
Jan Heinrich Merker <heinrich.merker@uni-jena.de>
2024-12-09 22:38:08 +0100

    Simplify retrieval modelling

--- axioms/model/retrieval.py
+++ axioms/model/retrieval.py
@@ -1,12 +1,17 @@
 from abc import abstractmethod, ABC
 from dataclasses import dataclass
 from math import log
-from typing import Union, FrozenSet, Sequence, Optional
+from typing import Union, FrozenSet, Sequence
 
 
 @dataclass(frozen=True)
 class Query:
-    title: str
+    id: str
+
+
+@dataclass(frozen=True)
+class TextQuery(Query):
+    text: str
 
 
 @dataclass(frozen=True)
@@ -16,17 +21,22 @@ class Document:
 
 @dataclass(frozen=True)
 class TextDocument(Document):
-    contents: str
+    text: str
 
 
 @dataclass(frozen=True)
 class ScoredDocument(Document):
     score: float
 
+
 @dataclass(frozen=True)
 class RankedDocument(Document):
-    score: float
     rank: int
+
+
+@dataclass(frozen=True)
+class ScoredTextDocument(ScoredDocument, TextDocument):
+    pass
 
 
 @dataclass(frozen=True)
@@ -55,8 +65,8 @@ class IndexContext(ABC):
         pass
 
     def contents(self, query_or_document: Union[Query, Document]) -> str:
-        if isinstance(query_or_document, Query):
-            return query_or_document.title
+        if isinstance(query_or_document, TextQuery):
+            return query_or_document.text
         elif isinstance(query_or_document, Document):
             return self.document_contents(query_or_document)
         else:
@@ -83,61 +93,3 @@ class IndexContext(ABC):
             return 0
         term_count = sum(1 for other in terms if other == term)
         return term_count / terms_len
-
-
-@dataclass(unsafe_hash=True)
-class _DefaultIndexContext(IndexContext):
-    """
-    Default lazy wrapper implementation to use as the global index context for all retrieval axioms (and preconditions).
-    """
-
-    context: Optional[IndexContext] = None
-
-    @property
-    def _context(self) -> IndexContext:
-        if self.context is None:
-            raise RuntimeError(
-                "Must set an index context before using the default index context."
-            )
-        return self.context
-
-    @property
-    def document_count(self) -> int:
-        return self._context.document_count
-
-    def document_frequency(self, term: str) -> int:
-        return self._context.document_frequency(term)
-
-    def inverse_document_frequency(self, term: str) -> float:
-        return self._context.inverse_document_frequency(term)
-
-    def document_contents(self, document: Document) -> str:
-        return self._context.document_contents(document)
-
-    def contents(self, query_or_document: Union[Query, Document]) -> str:
-        return self._context.contents(query_or_document)
-
-    def terms(self, query_or_document: Union[Query, Document]) -> Sequence[str]:
-        return self._context.terms(query_or_document)
-
-    def term_set(self, query_or_document: Union[Query, Document]) -> FrozenSet[str]:
-        return self._context.term_set(query_or_document)
-
-    def document_length(self, document: Document):
-        return self._context.document_length(document)
-
-    def term_frequency(
-        self, query_or_document: Union[Query, Document], term: str
-    ) -> float:
-        return self._context.term_frequency(query_or_document, term)
-
-
-_DEFAULT_INDEX_CONTEXT = _DefaultIndexContext()
-
-
-def get_index_context() -> IndexContext:
-    return _DEFAULT_INDEX_CONTEXT
-
-
-def set_index_context(context: IndexContext) -> None:
-    _DEFAULT_INDEX_CONTEXT.context = context
