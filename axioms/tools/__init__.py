fcd41908ba04c79f839c3db43a8f5f8b692cc65c
Jan Heinrich Merker <heinrich.merker@uni-jena.de>
2024-12-09 22:33:24 +0100

    Restructure tools and make tools injectable

--- axioms/tools/__init__.py
+++ axioms/tools/__init__.py
@@ -1,18 +1,70 @@
+from injector import Module, Binder
+
 # Re-export from sub-modules.
 
 from axioms.tools.aspects import (  # noqa: F401
     AspectExtraction,
     SpacyNounChunksAspectExtraction,
+    AspectsModule,
 )
+
+from axioms.tools.contents import (  # noqa: F401
+    TextContents,
+    DocumentQueryTextContents,
+    IrdsDocumentTextContents,
+    IrdsQueryTextContents,
+    AnseriniDocumentTextContents,
+    TerrierDocumentTextContents,
+    HasText,
+    SimpleTextContents,
+    ContentsModule,
+)
+
+from axioms.tools.index_statistics import (  # noqa: F401
+    IndexStatistics,
+    AnseriniIndexStatistics,
+    TerrierIndexStatistics,
+)
+
 from axioms.tools.pivot import (  # noqa: F401
     PivotSelection,
     RandomPivotSelection,
     FirstPivotSelection,
     LastPivotSelection,
     MiddlePivotSelection,
+    PivotModule,
 )
 
-from axioms.tools.term_similarity import (  # noqa: F401
+from axioms.tools.similarity import (  # noqa: F401
     TermSimilarity,
     WordNetSynonymSetTermSimilarity,
+    SimilarityModule,
 )
+
+from axioms.tools.text_statistics import (  # noqa: F401
+    TextStatistics,
+    DocumentQueryTextStatistics,
+    AnseriniTextStatistics,
+    TerrierTextStatistics,
+    SimpleTextStatistics,
+    TextStatisticsModule,
+)
+
+from axioms.tools.tokenizer import (  # noqa: F401
+    TermTokenizer,
+    NltkTermTokenizer,
+    AnseriniTermTokenizer,
+    TerrierTermTokenizer,
+    TokenizerModule,
+)
+
+
+class ToolsModule(Module):
+    def configure(self, binder: Binder) -> None:
+        binder.install(AspectsModule)
+        binder.install(ContentsModule)
+        binder.install(PivotModule)
+        binder.install(SimilarityModule)
+        binder.install(TokenizerModule)
+        # Need to be loaded after the tokenizer module because it needs it.
+        binder.install(TextStatisticsModule)
