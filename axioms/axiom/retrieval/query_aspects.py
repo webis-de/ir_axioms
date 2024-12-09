a321d43af30e77f4a08025c5dc23fefb33ef6d6a
Jan Heinrich Merker <heinrich.merker@uni-jena.de>
2024-12-09 22:34:24 +0100

    Refactor query aspects axioms to use injected tools

--- axioms/axiom/retrieval/query_aspects.py
+++ axioms/axiom/retrieval/query_aspects.py
@@ -1,20 +1,28 @@
-from dataclasses import dataclass
-from typing import Final, Set, FrozenSet, List
+from dataclasses import dataclass, field
+from typing import Final, Set, AbstractSet, List, Union
+
+from injector import inject, NoInject
 
 from axioms.axiom.base import Axiom
-from axioms.model.retrieval import get_index_context
+from axioms.axiom.precondition import PreconditionMixin
+from axioms.dependency_injection import injector
+from axioms.precondition.base import Precondition
 from axioms.precondition.length import LEN
 from axioms.axiom.utils import strictly_greater, approximately_equal
-from axioms.model import Query, Document, IndexContext
+from axioms.model import Query, Document, Preference
 from axioms.tools import (
     TermSimilarity,
-    WordNetSynonymSetTermSimilarity,
+    TextContents,
+    TermTokenizer,
+    IndexStatistics,
+    TextStatistics,
 )
+from axioms.utils.lazy import lazy_inject
 
 
 def _vocabulary_overlap(
-    vocabulary1: FrozenSet[str],
-    vocabulary2: FrozenSet[str],
+    vocabulary1: AbstractSet[str],
+    vocabulary2: AbstractSet[str],
 ):
     """
     Vocabulary overlap as calculated by the Jaccard coefficient.
@@ -27,43 +35,45 @@ def _vocabulary_overlap(
     )
 
 
+@inject
 @dataclass(frozen=True, kw_only=True)
-class RegAxiom(Axiom[Query, Document]):
+class RegAxiom(PreconditionMixin[Query, Document], Axiom[Query, Document]):
     """
     Reference:
     Zheng, W., Fang, H.: Query aspect based term weighting regularization
     in information retrieval. In: Gurrin, C., et al. (eds.) ECIR 2010.
     """
 
-    context: IndexContext
+    text_contents: TextContents[Union[Query, Document]]
+    term_tokenizer: TermTokenizer
+    text_statistics: TextStatistics[Document]
     term_similarity: TermSimilarity
+    precondition: NoInject[Precondition[Query, Document]] = field(default_factory=LEN)
 
     def preference(
         self,
         input: Query,
         output1: Document,
         output2: Document,
-    ):
-        query_terms = self.context.term_set(input)
+    ) -> Preference:
+        query_terms = set(self.term_tokenizer.terms(self.text_contents.contents(input)))
 
         minimum_similarity_term = self.term_similarity.least_similar_term(query_terms)
         if minimum_similarity_term is None:
             return 0
 
         return strictly_greater(
-            self.context.term_frequency(output1, minimum_similarity_term),
-            self.context.term_frequency(output2, minimum_similarity_term),
+            self.text_statistics.term_frequency(output1, minimum_similarity_term),
+            self.text_statistics.term_frequency(output2, minimum_similarity_term),
         )
 
 
-REG: Final = RegAxiom(
-    context=get_index_context(),
-    term_similarity=WordNetSynonymSetTermSimilarity(),
-).with_precondition(LEN)
+REG: Final = lazy_inject(RegAxiom, injector)
 
 
+@inject
 @dataclass(frozen=True, kw_only=True)
-class AntiRegAxiom(Axiom[Query, Document]):
+class AntiRegAxiom(PreconditionMixin[Query, Document], Axiom[Query, Document]):
     """
     Reference:
     Zheng, W., Fang, H.: Query aspect based term weighting regularization
@@ -72,33 +82,39 @@ class AntiRegAxiom(Axiom[Query, Document]):
     Modified to use maximum similarity instead of minimum similarity.
     """
 
-    context: IndexContext
+    text_contents: TextContents[Union[Query, Document]]
+    term_tokenizer: TermTokenizer
+    text_statistics: TextStatistics[Document]
     term_similarity: TermSimilarity
+    precondition: NoInject[Precondition[Query, Document]] = field(default_factory=LEN)
 
     def preference(
         self,
         input: Query,
         output1: Document,
         output2: Document,
-    ):
-        query_terms = self.context.term_set(input)
+    ) -> Preference:
+        query_terms = self.term_tokenizer.terms(
+            self.text_contents.contents(input),
+        )
+        query_unique_terms = set(query_terms)
 
-        maximum_similarity_term = self.term_similarity.most_similar_term(query_terms)
-        if maximum_similarity_term is None:
+        most_similarity_term = self.term_similarity.most_similar_term(
+            query_unique_terms
+        )
+        if most_similarity_term is None:
             return 0
 
         return strictly_greater(
-            self.context.term_frequency(output1, maximum_similarity_term),
-            self.context.term_frequency(output2, maximum_similarity_term),
+            self.text_statistics.term_frequency(output1, most_similarity_term),
+            self.text_statistics.term_frequency(output2, most_similarity_term),
         )
 
 
-ANTI_REG: Final = AntiRegAxiom(
-    context=get_index_context(),
-    term_similarity=WordNetSynonymSetTermSimilarity(),
-)
+ANTI_REG: Final = lazy_inject(AntiRegAxiom, injector)
 
 
+@inject
 @dataclass(frozen=True, kw_only=True)
 class AspectRegAxiom(Axiom[Query, Document]):
     """
@@ -111,7 +127,9 @@ class AspectRegAxiom(Axiom[Query, Document]):
     in information retrieval. In: Gurrin, C., et al. (eds.) ECIR 2010.
     """
 
-    context: IndexContext
+    text_contents: TextContents[Union[Query, Document]]
+    term_tokenizer: TermTokenizer
+    index_statistics: IndexStatistics
     term_similarity: TermSimilarity
     term_discriminator_margin_fraction: float = 0.1
 
@@ -120,15 +138,26 @@ class AspectRegAxiom(Axiom[Query, Document]):
         input: Query,
         output1: Document,
         output2: Document,
-    ):
-        query_terms = self.context.term_set(input)
-        document1_terms = self.context.term_set(output1)
-        document2_terms = self.context.term_set(output2)
-        if len(query_terms) == 0:
+    ) -> Preference:
+        query_terms = self.term_tokenizer.terms(
+            self.text_contents.contents(input),
+        )
+        query_unique_terms = set(query_terms)
+        document1_terms = self.term_tokenizer.terms(
+            self.text_contents.contents(output1),
+        )
+        document1_unique_terms = set(document1_terms)
+        document2_terms = self.term_tokenizer.terms(
+            self.text_contents.contents(output2),
+        )
+        document2_unique_terms = set(document2_terms)
+
+        if len(query_unique_terms) == 0:
             return 0
 
         term_discriminators = {
-            self.context.inverse_document_frequency(term) for term in query_terms
+            self.index_statistics.inverse_document_frequency(term)
+            for term in query_unique_terms
         }
         if not approximately_equal(
             *term_discriminators, self.term_discriminator_margin_fraction
@@ -137,10 +166,10 @@ class AspectRegAxiom(Axiom[Query, Document]):
             return 0
 
         average_similarity = self.term_similarity.average_similarity(
-            query_terms, query_terms
+            query_unique_terms, query_unique_terms
         )
 
-        query_aspects: List[Set[str]] = [{term} for term in query_terms]
+        query_aspects: List[Set[str]] = [{term} for term in query_unique_terms]
 
         # Iterate aspect 1 from start.
         for i1 in range(0, len(query_aspects) - 1, +1):
@@ -161,10 +190,14 @@ class AspectRegAxiom(Axiom[Query, Document]):
                     query_aspects.pop(i2)
 
         count_document1_aspects = {
-            1 for aspect in query_aspects if not document1_terms.isdisjoint(aspect)
+            1
+            for aspect in query_aspects
+            if not document1_unique_terms.isdisjoint(aspect)
         }
         count_document2_aspects = {
-            1 for aspect in query_aspects if not document2_terms.isdisjoint(aspect)
+            1
+            for aspect in query_aspects
+            if not document2_unique_terms.isdisjoint(aspect)
         }
         return strictly_greater(
             len(count_document1_aspects) > 0,
@@ -172,38 +205,52 @@ class AspectRegAxiom(Axiom[Query, Document]):
         )
 
 
-ASPECT_REG: Final = AspectRegAxiom(
-    context=get_index_context(),
-    term_similarity=WordNetSynonymSetTermSimilarity(),
-)
+ASPECT_REG: Final = lazy_inject(AspectRegAxiom, injector)
 
 
+@inject
 @dataclass(frozen=True, kw_only=True)
 class AndAxiom(Axiom[Query, Document]):
-    context: IndexContext
+    text_contents: TextContents[Union[Query, Document]]
+    term_tokenizer: TermTokenizer
 
     def preference(
         self,
         input: Query,
         output1: Document,
         output2: Document,
-    ):
-        query_terms = self.context.term_set(input)
-        document1_terms = self.context.term_set(output1)
-        document2_terms = self.context.term_set(output2)
-        query_term_count1 = query_terms & document1_terms
-        query_term_count2 = query_terms & document2_terms
-        all_query_terms1 = len(query_term_count1) == len(query_terms)
-        all_query_terms2 = len(query_term_count2) == len(query_terms)
+    ) -> Preference:
+        query_terms = self.term_tokenizer.terms(
+            self.text_contents.contents(input),
+        )
+        query_unique_terms = set(query_terms)
+        document1_terms = self.term_tokenizer.terms(
+            self.text_contents.contents(output1),
+        )
+        document1_unique_terms = set(document1_terms)
+        document2_terms = self.term_tokenizer.terms(
+            self.text_contents.contents(output2),
+        )
+        document2_unique_terms = set(document2_terms)
+
+        all_query_terms1 = query_unique_terms.issubset(document1_unique_terms)
+        all_query_terms2 = query_unique_terms.issubset(document2_unique_terms)
         return strictly_greater(all_query_terms1, all_query_terms2)
 
 
-AND: Final = AndAxiom(
-    context=get_index_context(),
-)
-LEN_AND: Final = AND.with_precondition(LEN)
+AND: Final = lazy_inject(AndAxiom, injector)
 
 
+@inject
+@dataclass(frozen=True, kw_only=True)
+class LenAndAxiom(PreconditionMixin[Query, Document], AndAxiom):
+    precondition: NoInject[Precondition[Query, Document]] = field(default_factory=LEN)
+
+
+LEN_AND: Final = lazy_inject(LenAndAxiom, injector)
+
+
+@inject
 @dataclass(frozen=True, kw_only=True)
 class ModifiedAndAxiom(Axiom[Query, Document]):
     """
@@ -211,47 +258,89 @@ class ModifiedAndAxiom(Axiom[Query, Document]):
     One document contains a larger subset of query terms.
     """
 
-    context: IndexContext
+    text_contents: TextContents[Union[Query, Document]]
+    term_tokenizer: TermTokenizer
 
     def preference(
         self,
         input: Query,
         output1: Document,
         output2: Document,
-    ):
-        query_terms = self.context.term_set(input)
-        document1_terms = self.context.term_set(output1)
-        document2_terms = self.context.term_set(output2)
-        query_term_count1 = query_terms & document1_terms
-        query_term_count2 = query_terms & document2_terms
+    ) -> Preference:
+        query_terms = self.term_tokenizer.terms(
+            self.text_contents.contents(input),
+        )
+        query_unique_terms = set(query_terms)
+        document1_terms = self.term_tokenizer.terms(
+            self.text_contents.contents(output1),
+        )
+        document1_unique_terms = set(document1_terms)
+        document2_terms = self.term_tokenizer.terms(
+            self.text_contents.contents(output2),
+        )
+        document2_unique_terms = set(document2_terms)
 
+        query_term_count1 = query_unique_terms & document1_unique_terms
+        query_term_count2 = query_unique_terms & document2_unique_terms
         return strictly_greater(len(query_term_count1), len(query_term_count2))
 
 
-M_AND: Final = ModifiedAndAxiom(
-    context=get_index_context(),
-)
-LEN_M_AND: Final = M_AND.with_precondition(LEN)
+M_AND: Final = lazy_inject(ModifiedAndAxiom, injector)
 
 
+@inject
+@dataclass(frozen=True, kw_only=True)
+class LenModifiedAndAxiom(PreconditionMixin[Query, Document], ModifiedAndAxiom):
+    precondition: NoInject[Precondition[Query, Document]] = field(default_factory=LEN)
+
+
+LEN_M_AND: Final = lazy_inject(LenModifiedAndAxiom, injector)
+
+
+@inject
 @dataclass(frozen=True, kw_only=True)
 class DivAxiom(Axiom[Query, Document]):
-    context: IndexContext
+    text_contents: TextContents[Union[Query, Document]]
+    term_tokenizer: TermTokenizer
 
     def preference(
         self,
         input: Query,
         output1: Document,
         output2: Document,
-    ):
-        query_terms = self.context.term_set(input)
-        overlap1 = _vocabulary_overlap(query_terms, self.context.term_set(output1))
-        overlap2 = _vocabulary_overlap(query_terms, self.context.term_set(output2))
+    ) -> Preference:
+        query_terms = self.term_tokenizer.terms(
+            self.text_contents.contents(input),
+        )
+        query_unique_terms = set(query_terms)
+        document1_terms = self.term_tokenizer.terms(
+            self.text_contents.contents(output1),
+        )
+        document1_unique_terms = set(document1_terms)
+        document2_terms = self.term_tokenizer.terms(
+            self.text_contents.contents(output2),
+        )
+        document2_unique_terms = set(document2_terms)
+
+        overlap1 = _vocabulary_overlap(
+            vocabulary1=query_unique_terms,
+            vocabulary2=document1_unique_terms,
+        )
+        overlap2 = _vocabulary_overlap(
+            vocabulary1=query_unique_terms,
+            vocabulary2=document2_unique_terms,
+        )
 
         return strictly_greater(overlap2, overlap1)
 
 
-DIV: Final = DivAxiom(
-    context=get_index_context(),
-)
-LEN_DIV: Final = DIV.with_precondition(LEN)
+DIV: Final = lazy_inject(DivAxiom, injector)
+
+
+@inject
+@dataclass(frozen=True, kw_only=True)
+class LenDivAxiom(PreconditionMixin[Query, Document], DivAxiom):
+    precondition: NoInject[Precondition[Query, Document]] = field(default_factory=LEN)
+
+
+LEN_DIV: Final = lazy_inject(LenDivAxiom, injector)
