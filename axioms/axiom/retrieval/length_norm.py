e1967209cdb5104a681a41741f4a1fb53cc28f14
Jan Heinrich Merker <heinrich.merker@uni-jena.de>
2024-12-09 22:34:13 +0100

    Refactor length norm axioms to use injected tools

--- axioms/axiom/retrieval/length_norm.py
+++ axioms/axiom/retrieval/length_norm.py
@@ -1,66 +1,87 @@
 from dataclasses import dataclass
-from typing import Final
+from typing import Final, Union
+
+from injector import inject
 
 from axioms.axiom.base import Axiom
-from axioms.axiom.utils import approximately_equal, strictly_less, strictly_greater
-from axioms.model import Query, Document, IndexContext
-from axioms.model.retrieval import get_index_context
+from axioms.dependency_injection import injector
+from axioms.axiom.utils import approximately_equal, strictly_fewer, strictly_greater
+from axioms.model import Query, Document, Preference
+from axioms.tools import TextContents, TermTokenizer, TextStatistics
+from axioms.utils.lazy import lazy_inject
 
 
+@inject
 @dataclass(frozen=True, kw_only=True)
 class Lnc1Axiom(Axiom[Query, Document]):
-    context: IndexContext
+    text_contents: TextContents[Union[Query, Document]]
+    term_tokenizer: TermTokenizer
+    text_statistics: TextStatistics[Document]
 
     def preference(
         self,
         input: Query,
         output1: Document,
         output2: Document,
-    ):
+    ) -> Preference:
+        query_terms = self.term_tokenizer.terms(
+            self.text_contents.contents(input),
+        )
         if not all(
             approximately_equal(
-                self.context.term_frequency(output1, term),
-                self.context.term_frequency(output2, term),
+                self.text_statistics.term_frequency(output1, term),
+                self.text_statistics.term_frequency(output2, term),
             )
-            for term in self.context.term_set(input)
+            for term in set(query_terms)
         ):
             return 0
 
-        # Prefer the shorter document.
-        return strictly_less(
-            len(self.context.terms(output1)),
-            len(self.context.terms(output2)),
+        document1_terms = self.term_tokenizer.terms(
+            self.text_contents.contents(output1),
+        )
+        document2_terms = self.term_tokenizer.terms(
+            self.text_contents.contents(output2),
         )
 
-
-LNC1: Final = Lnc1Axiom(
-    context=get_index_context(),
-)
+        # Prefer the shorter document.
+        return strictly_fewer(document1_terms, document2_terms)
 
 
+LNC1: Final = lazy_inject(Lnc1Axiom, injector)
+
+
+@inject
 @dataclass(frozen=True, kw_only=True)
 class TfLncAxiom(Axiom[Query, Document]):
-    context: IndexContext
+    text_contents: TextContents[Union[Query, Document]]
+    term_tokenizer: TermTokenizer
+    text_statistics: TextStatistics[Document]
 
     def preference(
         self,
         input: Query,
         output1: Document,
         output2: Document,
-    ):
+    ) -> Preference:
+        query_terms = self.term_tokenizer.terms(
+            self.text_contents.contents(input),
+        )
+        document1_terms = self.term_tokenizer.terms(
+            self.text_contents.contents(output1),
+        )
+        document2_terms = self.term_tokenizer.terms(
+            self.text_contents.contents(output2),
+        )
+
         sum_document1 = 0
         sum_document2 = 0
 
-        for query_term in self.context.term_set(input):
-            tf_d1 = self.context.term_frequency(output1, query_term)
-            tf_d2 = self.context.term_frequency(output2, query_term)
+        for query_term in set(query_terms):
+            tf_d1 = self.text_statistics.term_frequency(output1, query_term)
+            tf_d2 = self.text_statistics.term_frequency(output2, query_term)
 
-            len_d1 = len(
-                [term for term in self.context.terms(output1) if term != query_term]
-            )
-            len_d2 = len(
-                [term for term in self.context.terms(output2) if term != query_term]
-            )
+            len_d1 = sum(1 for term in document1_terms if term != query_term)
+            len_d2 = sum(1 for term in document2_terms if term != query_term)
 
             if len_d1 == len_d2:
                 if tf_d1 > tf_d2:
@@ -71,6 +92,4 @@ class TfLncAxiom(Axiom[Query, Document]):
         return strictly_greater(sum_document1, sum_document2)
 
 
-TF_LNC: Final = TfLncAxiom(
-    context=get_index_context(),
-)
+TF_LNC: Final = lazy_inject(TfLncAxiom, injector)
