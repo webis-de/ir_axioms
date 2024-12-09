5937624ec6d1601c84a5bd47724ee236524e9edc
Jan Heinrich Merker <heinrich.merker@uni-jena.de>
2024-12-09 22:34:41 +0100

    Refactor lower bound axioms to use injected tools

--- axioms/axiom/retrieval/lower_bound.py
+++ axioms/axiom/retrieval/lower_bound.py
@@ -1,30 +1,42 @@
 from dataclasses import dataclass
 from math import isclose
-from typing import Final
+from typing import Final, Union
+
+from injector import inject
 
 from axioms.axiom.base import Axiom
 from axioms.axiom.utils import approximately_equal
-from axioms.model import Query, ScoredDocument, IndexContext
-from axioms.model.retrieval import get_index_context
+from axioms.dependency_injection import injector
+from axioms.model import Query, Document, ScoredDocument, Preference
+from axioms.tools import TextContents, TermTokenizer, TextStatistics
+from axioms.utils.lazy import lazy_inject
 
 
+@inject
 @dataclass(frozen=True, kw_only=True)
 class Lb1Axiom(Axiom[Query, ScoredDocument]):
-    context: IndexContext
+    text_contents: TextContents[Union[Query, Document]]
+    term_tokenizer: TermTokenizer
+    text_statistics: TextStatistics[Document]
 
     def preference(
         self,
         input: Query,
         output1: ScoredDocument,
         output2: ScoredDocument,
-    ):
+    ) -> Preference:
         if not approximately_equal(output1.score, output2.score):
             return 0
 
+        query_terms = self.term_tokenizer.terms(
+            self.text_contents.contents(input),
+        )
+
         # TODO: Do we really want to use the term set here, not the list?
-        for term in self.context.term_set(input):
-            tf1 = self.context.term_frequency(output1, term)
-            tf2 = self.context.term_frequency(output2, term)
+        #  It seems as if the order of the terms should matter.
+        for term in set(query_terms):
+            tf1 = self.text_statistics.term_frequency(output1, term)
+            tf2 = self.text_statistics.term_frequency(output2, term)
             if isclose(tf1, 0) and tf2 > 0:
                 return -1
             if isclose(tf2, 0) and tf1 > 0:
@@ -32,6 +44,4 @@ class Lb1Axiom(Axiom[Query, ScoredDocument]):
         return 0
 
 
-LB1: Final = Lb1Axiom(
-    context=get_index_context(),
-)
+LB1: Final = lazy_inject(Lb1Axiom, injector)
