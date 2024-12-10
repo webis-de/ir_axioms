64372d9ff6a78b6a4ca03102b63c5de6e3a6965d
Jan Heinrich Merker <heinrich.merker@uni-jena.de>
2024-12-10 13:13:09 +0100

    Fix tests

--- tests/test_term_similarity.py
+++ tests/test_term_similarity.py
@@ -1,4 +1,4 @@
-from axioms.axiom import STMC1  #, STMC2
+from axioms.axiom import STMC1, STMC2
 from axioms.model import TextQuery, TextDocument
 from tests.util import inject_documents
 
@@ -17,45 +17,44 @@ def test_stmc1():
     assert axiom.preference(query, document2, document1) == -1
 
 
-# FIXME: The below tests fail now because of the changed term similarity implementation. We should mock the term similarity here and enable the checks again.
-# def test_stmc2():
-#     query = TextQuery("q1", "q")
-#     document1 = TextDocument("d1", "q")
-#     document2 = TextDocument("d2", "t t t t")
+def test_stmc2():
+    query = TextQuery("q1", "car")
+    document1 = TextDocument("d1", "car key")
+    document2 = TextDocument("d2", "auto auto auto auto")
 
-#     inject_documents([document1, document2])
+    inject_documents([document1, document2])
 
-#     axiom = STMC2()
+    axiom = STMC2()
 
-#     # Document d1 contains an exact match.
-#     assert axiom.preference(query, document1, document2) == 1
-#     assert axiom.preference(query, document2, document1) == -1
+    # Document d1 contains an exact match.
+    assert axiom.preference(query, document1, document2) == 1
+    assert axiom.preference(query, document2, document1) == -1
 
 
-# def test_stmc2_equal():
-#     query = TextQuery("q1", "dog breed")
-#     document1 = TextDocument("d1", "dog fire orange")
-#     document2 = TextDocument("d2", "dog animal animal animal time key")
-#     document3 = TextDocument("d3", "dog animal time key")
+def test_stmc2_equal():
+    query = TextQuery("q1", "dog breed")
+    document1 = TextDocument("d1", "dog fire orange key")
+    document2 = TextDocument("d2", "animal animal animal animal time key key key")
+    document3 = TextDocument("d3", "dog animal time key key")
 
-#     inject_documents([document1, document2])
+    inject_documents([document1, document2])
 
-#     axiom = STMC2()
+    axiom = STMC2()
 
-#     # Most similar query term 'dog' and non-query term 'animal'.
-#     # The document 2 non-query term frequency (0.5)
-#     # compared to the document 1 query term frequency (0.333)
-#     # is similar to the document 2 term set length (4)
-#     # compared to the document 1 term set length (3):
-#     # 1.333 ≈ 1.5
-#     assert axiom.preference(query, document1, document2) == 1
-#     assert axiom.preference(query, document2, document1) == -1
+    # Most similar query term 'dog' and non-query term 'animal'.
+    # The document 2 non-query term frequency (0.5)
+    # compared to the document 1 query term frequency (0.25)
+    # is similar to the document 2 length (8)
+    # compared to the document 1 length (4):
+    # 2 ≈ 2
+    assert axiom.preference(query, document1, document2) == 1
+    assert axiom.preference(query, document2, document1) == -1
 
-#     # Most similar query term 'dog' and non-query term 'animal'.
-#     # The document 3 non-query term frequency (0.25)
-#     # compared to the document 1 query term frequency (0.333)
-#     # is not similar to the document 3 term set length (4)
-#     # compared to the document 1 term set length (3):
-#     # 1.333 != 0.75
-#     assert axiom.preference(query, document1, document3) == 0
-#     assert axiom.preference(query, document3, document1) == 0
+    # Most similar query term 'dog' and non-query term 'animal'.
+    # The document 3 non-query term frequency (0.2)
+    # compared to the document 1 query term frequency (0.25)
+    # is not similar to the document 3 length (5)
+    # compared to the document 1 length (4):
+    # 0.8 != 1.25
+    assert axiom.preference(query, document1, document3) == 0
+    assert axiom.preference(query, document3, document1) == 0
