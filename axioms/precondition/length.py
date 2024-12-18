139e114cb0caada4d741ca9804a2dfdc545180ce
Jan Heinrich Merker <heinrich.merker@uni-jena.de>
2024-12-18 23:19:39 +0100

    Improve LEN precondistion efficiency

--- axioms/precondition/length.py
+++ axioms/precondition/length.py
@@ -1,11 +1,12 @@
 from dataclasses import dataclass
-from typing import Final, TypeVar
+from typing import Final, Sequence, TypeVar
 
 from injector import inject
+from numpy import array, bool_
 
 from axioms.axiom.utils import approximately_equal
 from axioms.dependency_injection import injector
-from axioms.model import Document, Mask
+from axioms.model import Document, Mask, MaskMatrix
 from axioms.precondition.base import Precondition
 from axioms.tools import TextContents, TermTokenizer
 from axioms.utils.lazy import lazy_inject
@@ -31,6 +32,28 @@ class LenPrecondition(Precondition[Input, Document]):
             len(self.term_tokenizer.terms(self.text_contents.contents(output2))),
             margin_fraction=self.margin_fraction,
         )
+
+    def preconditions(
+        self,
+        input: Input,
+        outputs: Sequence[Document],
+    ) -> MaskMatrix:
+        lengths = [
+            len(self.term_tokenizer.terms(self.text_contents.contents(output)))
+            for output in outputs
+        ]
+        return array(
+            [
+                approximately_equal(
+                    length1,
+                    length2,
+                    margin_fraction=self.margin_fraction,
+                )
+                for length1 in lengths
+                for length2 in lengths
+            ],
+            dtype=bool_,
+        ).reshape((len(outputs), len(outputs)))
 
 
 LEN: Final = lazy_inject(LenPrecondition, injector)
