f21e5d42afa3d090a3182aa543b0d186bfc1eb19
Jan Heinrich Merker <heinrich.merker@uni-jena.de>
2024-12-18 13:39:00 +0100

    Improve caching

--- axioms/axiom/cache.py
+++ axioms/axiom/cache.py
@@ -1,13 +1,15 @@
 from dataclasses import dataclass
 from dbm import open as dbm_open
 from functools import cached_property
+from math import nan
 from pathlib import Path
 from sqlite3 import connect
 from struct import pack, unpack
 from typing import Iterable, Iterator, Protocol, Sequence, Tuple, TypeVar
 
-from numpy import array, float_
+from numpy import array, float_, ndarray, isnan, ones_like
 from more_itertools import chunked
+from optimask import OptiMask
 from typing_extensions import TypeAlias  # type: ignore
 
 from axioms.axiom.base import Axiom
@@ -89,6 +91,7 @@ class SqliteCachedAxiom(Axiom[_Input, _Output]):
         self,
         inputs_outputs: Iterable[Tuple[_Input, _Output, _Output]],
     ) -> Iterator[Preference]:
+        self.cache_path.parent.mkdir(exist_ok=True, parents=True)
         for chunk in chunked(inputs_outputs, 100):
             yield from self._preferences_batch(chunk)
 
@@ -98,9 +101,11 @@ class SqliteCachedAxiom(Axiom[_Input, _Output]):
         output1: _Output,
         output2: _Output,
     ) -> Preference:
-        return self._preferences_batch(
-            inputs_outputs=[(input, output1, output2)],
-        )[0]
+        return next(
+            self._iter_preferences(
+                inputs_outputs=[(input, output1, output2)],
+            )
+        )
 
     def preferences(
         self,
@@ -133,7 +138,9 @@ class DbmCachedAxiom(Axiom[_Input, _Output]):
     def _iter_preferences(
         self,
         inputs_outputs: Iterable[Tuple[_Input, _Output, _Output]],
+        only_cached: bool = False,
     ) -> Iterator[Preference]:
+        self.cache_path.parent.mkdir(exist_ok=True, parents=True)
         with dbm_open(self.cache_path, flag="c") as cache:
             for input, output1, output2 in inputs_outputs:
                 key = repr((input, output1, output2)).encode(encoding="utf-8")
@@ -141,6 +148,8 @@ class DbmCachedAxiom(Axiom[_Input, _Output]):
                     preference_bytes = cache[key]
                     (preference,) = unpack("f", preference_bytes)
                     yield preference
+                elif only_cached:
+                    yield nan
                 else:
                     preference = self.axiom.preference(input, output1, output2)
                     preference_bytes = pack("f", preference)
@@ -159,21 +168,75 @@ class DbmCachedAxiom(Axiom[_Input, _Output]):
             )
         )
 
+    def _cached_preferences(
+        self,
+        input: _Input,
+        outputs: Sequence[_Output],
+    ) -> ndarray:
+        return array(
+            list(
+                self._iter_preferences(
+                    (
+                        (input, output1, output2)
+                        for output2 in outputs
+                        for output1 in outputs
+                    ),
+                    only_cached=True,
+                )
+            ),
+            dtype=float_,
+        ).reshape((len(outputs), len(outputs)))
+
+    @cached_property
+    def _opti_mask(self) -> OptiMask:
+        return OptiMask()
+
     def preferences(
         self,
         input: _Input,
         outputs: Sequence[_Output],
     ) -> PreferenceMatrix:
-        return array(
-            list(
-                self._iter_preferences(
-                    (input, output1, output2)
-                    for output2 in outputs
-                    for output1 in outputs
-                )
-            ),
-            dtype=float_,
-        ).reshape((len(outputs), len(outputs)))
+        # First, only load the cached dependencies.
+        preferences = self._cached_preferences(input, outputs)
+
+        # Then, fill as-large-as-possible sub-blocks of the matrix by computing preference matrices. 
+        while isnan(preferences).any():
+            mask = ones_like(preferences)
+            mask[~isnan(preferences)] = nan
+            rows, cols = self._opti_mask.solve(mask)
+            indices = sorted(set(rows) & set(cols))
+            if len(indices) == 0:
+                break
+            start = min(indices)
+            end = max(indices) + 1
+            sub_outputs = outputs[start:end]
+            sub_preferences = self.axiom.preferences(input, sub_outputs)
+
+            with dbm_open(self.cache_path, flag="c") as cache:
+                for i1, output1 in enumerate(sub_outputs):
+                    for i2, output2 in enumerate(sub_outputs):
+                        key = repr((input, output1, output2)).encode(encoding="utf-8")
+                        preference_bytes = pack("f", sub_preferences[i1, i2])
+                        cache[key] = preference_bytes
+
+            preferences[start:end, start:end] = sub_preferences
+
+        # Last, for the remaining pairs, fill them iteratively.
+        unfilled_pairs = [
+            (i1, i2)
+            for i1 in range(len(outputs))
+            for i2 in range(len(outputs))
+            if isnan(preferences[i1,i2])
+        ]
+        unfilled_inouts_outputs = (
+            (input, outputs[i1], outputs[i2])
+            for i1, i2 in unfilled_pairs
+        )
+        unfilled_preferences = self._iter_preferences(unfilled_inouts_outputs)
+        for (i1, i2), preference in zip(unfilled_pairs, unfilled_preferences):
+            preferences[i1, i2] = preference
+
+        return preferences
 
     def cached(self, cache_path: Path) -> Axiom[_Input, _Output]:
         if self.cache_path == cache_path:
