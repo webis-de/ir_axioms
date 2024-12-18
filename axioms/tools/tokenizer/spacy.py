74a5b4c731b65269d1a99dc9b01b6d04d1aac7ce
Jan Heinrich Merker <heinrich.merker@uni-jena.de>
2024-12-18 23:16:41 +0100

    Improve spaCy term tokenizer

--- axioms/tools/tokenizer/spacy.py
+++ axioms/tools/tokenizer/spacy.py
@@ -1,9 +1,10 @@
 from dataclasses import dataclass
 from functools import cached_property
-from typing import Sequence
+from typing import Iterable, Sequence
 
 from spacy import load as spacy_load
 from spacy.language import Language
+from spacy.tokens import Token
 
 from axioms.tools.tokenizer.base import TermTokenizer, SentenceTokenizer
 
@@ -11,8 +12,10 @@ from axioms.tools.tokenizer.base import TermTokenizer, SentenceTokenizer
 @dataclass(frozen=True, kw_only=True)
 class SpacyTermTokenizer(TermTokenizer):
     language_name: str = "en_core_web_sm"
-    lemmatize: bool = False
-    lowercase: bool = False
+    lemmatize: bool = True
+    lowercase: bool = True
+    remove_punctuation: bool = True
+    remove_stopwords: bool = True
 
     @cached_property
     def _language(self) -> Language:
@@ -20,14 +23,22 @@ class SpacyTermTokenizer(TermTokenizer):
 
     def terms(self, text: str) -> Sequence[str]:
         document = self._language(text)
-        terms: Sequence[str]
+
+        tokens: Iterable[Token] = (token for token in document)
+        if self.remove_punctuation:
+            tokens = (token for token in tokens if not token.is_punct)
+        if self.remove_stopwords:
+            tokens = (token for token in tokens if not token.is_stop)
+
+        terms: Iterable[str]
         if self.lemmatize:
-            terms = [token.lemma_ for token in document]
+            terms = (token.lemma_ for token in tokens)
         else:
-            terms = [token.text for token in document]
+            terms = (token.text for token in tokens)
         if self.lowercase:
-            terms = [term.lower() for term in terms]
-        return terms
+            terms = (term.lower() for term in terms)
+
+        return list(terms)
 
 
 @dataclass(frozen=True, kw_only=True)
