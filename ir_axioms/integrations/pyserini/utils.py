4b9a8c0d37f75dbf70e3423a08c0dd3ed6a677ad
Jan Heinrich Merker <heinrich.merker@uni-jena.de>
2025-08-05 17:45:01 +0200

    Make index injection optional for PyTerrier and Pyserini

--- ir_axioms/integrations/pyserini/utils.py
+++ ir_axioms/integrations/pyserini/utils.py
@@ -30,51 +30,52 @@ if is_pyserini_installed() or TYPE_CHECKING:
     from ir_axioms.utils.pyserini import Analyzer, default_analyzer
 
     def inject_pyserini(
-        index_dir: Union[Path, str],
+        index_dir: Optional[Union[Path, str]] = None,
         analyzer: Optional[Analyzer] = None,
         text_contents: bool = False,
         dataset: Optional[Union[Dataset, str]] = None,
     ) -> None:
         analyzer_ = analyzer if analyzer is not None else default_analyzer()
-
-        injector.binder.bind(
-            interface=TextStatistics,
-            to=AnseriniTextStatistics(
-                index_dir=index_dir,
-                analyzer=analyzer_,
-            ),
-            scope=singleton,
-        )
         injector.binder.bind(
             interface=TermTokenizer,
             to=AnseriniTermTokenizer(analyzer=analyzer_),
             scope=singleton,
         )
-        injector.binder.bind(
-            interface=IndexStatistics,
-            to=AnseriniIndexStatistics(
-                index_dir=index_dir,
-                analyzer=analyzer_,
-            ),
-            scope=singleton,
-        )
 
-        if text_contents:
-
-            @inject
-            def _make_terrier_document_text_contents(
-                fallback_text_contents: TextContents[HasText],
-            ) -> TextContents[Document]:
-                return AnseriniDocumentTextContents(
-                    fallback_text_contents=fallback_text_contents,
-                    index_dir=index_dir,
-                )
-
+        if index_dir is not None:
             injector.binder.bind(
-                interface=TextContents[Document],
-                to=_make_terrier_document_text_contents,
+                interface=TextStatistics,
+                to=AnseriniTextStatistics(
+                    index_dir=index_dir,
+                    analyzer=analyzer_,
+                ),
                 scope=singleton,
             )
+            injector.binder.bind(
+                interface=IndexStatistics,
+                to=AnseriniIndexStatistics(
+                    index_dir=index_dir,
+                    analyzer=analyzer_,
+                ),
+                scope=singleton,
+            )
+
+            if text_contents:
+
+                @inject
+                def _make_terrier_document_text_contents(
+                    fallback_text_contents: TextContents[HasText],
+                ) -> TextContents[Document]:
+                    return AnseriniDocumentTextContents(
+                        fallback_text_contents=fallback_text_contents,
+                        index_dir=index_dir,
+                    )
+
+                injector.binder.bind(
+                    interface=TextContents[Document],
+                    to=_make_terrier_document_text_contents,
+                    scope=singleton,
+                )
 
         if dataset is not None:
 
