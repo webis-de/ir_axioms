375cfb69dbc4885d95522b9d60bfeff04f0eb986
Jan Heinrich Merker <heinrich.merker@uni-jena.de>
2025-08-05 18:20:11 +0200

    Improve documentation

--- ir_axioms/precondition/base.py
+++ ir_axioms/precondition/base.py
@@ -12,21 +12,38 @@ from ir_axioms.model.base import (
 
 
 class Precondition(Generic[Input, Output], Protocol):
+    """
+    A precondition can be used to restrict an axiom's preference computation to only those outputs that meet a certain condition (e.g., similar length).
+    """
+
     def precondition(
         self,
         input: Input,
         output1: Output,
         output2: Output,
     ) -> Mask:
-        # TODO: Documentation.
-        pass
+        """
+        Check if the precondition holds for a pair of outputs given an input.
+
+        :param input: Common input for both outputs.
+        :param output1: One output for the common input.
+        :param output2: Another output for the common input.
+        :return: ``True`` if a preference can be computed for the outputs, ``False`` otherwise.
+        """
+        ...
 
     def preconditions(
         self,
         input: Input,
         outputs: Sequence[Output],
     ) -> MaskMatrix:
-        # TODO: Documentation.
+        """
+        Check if the precondition holds for all pairs of outputs given an input.
+
+        :param input: Common input for all outputs.
+        :param outputs: The outputs for the common input.
+        :return: A mask matrix, where the ij-th entry corresponds to whether the precondition holds for the i-th and j-th output.
+        """
         return array(
             list(
                 tqdm(
