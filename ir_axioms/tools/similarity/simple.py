1c1f0285e101f0123c8f4b237a67ba2daa384792
Jan Heinrich Merker <heinrich.merker@uni-jena.de>
2025-08-05 18:38:07 +0200

    Add term-based sentence similarity fallback

---
+++ ir_axioms/tools/similarity/simple.py
@@ -0,0 +1,24 @@
+from dataclasses import dataclass
+from injector import inject
+
+
+from ir_axioms.tools.tokenizer.base import TermTokenizer
+from ir_axioms.tools.similarity.base import SentenceSimilarity, TermSimilarity
+
+
+@inject
+@dataclass(frozen=True)
+class AverageTermSimilaritySentenceSimilarity(SentenceSimilarity):
+    """
+    Sentence similarity based on the mean term similarity of the terms in the sentences.
+
+    Note: This is only meant as a fallback if no other sentence similarity model is available.
+    """
+
+    term_tokenizer: TermTokenizer
+    term_similarity: TermSimilarity
+
+    def similarity(self, sentence1: str, sentence2: str) -> float:
+        terms1 = self.term_tokenizer.terms(sentence1)
+        terms2 = self.term_tokenizer.terms(sentence2)
+        return self.term_similarity.average_similarity(terms1, terms2)
