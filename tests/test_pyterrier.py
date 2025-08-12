ab6db2996706cf3476aa52eeb65a99ad6141e6c2
Jan Heinrich Merker <heinrich.merker@uni-jena.de>
2025-08-12 21:04:54 +0200

    Add axiomatic experiment tests

--- tests/test_pyterrier.py
+++ tests/test_pyterrier.py
@@ -1,7 +1,7 @@
-from typing import Any
+from typing import Any, TYPE_CHECKING
 
 from approvaltests import verify
-from pytest import skip
+from pytest import skip, fixture
 
 from ir_axioms.axiom import Axiom
 from ir_axioms.model import Document
@@ -155,4 +155,105 @@ def test_aggregated_axiomatic_preferences_compute() -> None:
     verify(df_out.to_csv(path_or_buf=None, index=False, header=True))
 
 
-# TODO: Experiment, Estimator
+# TODO: Estimator
+
+if TYPE_CHECKING:
+    from ir_axioms.integrations import AxiomaticExperiment
+else:
+    AxiomaticExperiment = Any
+
+
+@fixture
+def axiomatic_experiment() -> AxiomaticExperiment:
+    if not is_pyterrier_installed():
+        skip("PyTerrier is not installed.")
+
+    from pandas import DataFrame
+    from pyterrier import Transformer
+
+    from ir_axioms.integrations import AxiomaticExperiment
+
+    return AxiomaticExperiment(
+        retrieval_systems=[
+            Transformer.from_df(
+                DataFrame(
+                    [
+                        {"qid": "q1", "docno": "doc2", "score": 0.5, "rank": 0},
+                        {"qid": "q1", "docno": "doc1", "score": 0.3, "rank": 1},
+                        {"qid": "q2", "docno": "doc1", "score": 0.4, "rank": 0},
+                        {"qid": "q2", "docno": "doc2", "score": 0.2, "rank": 1},
+                    ]
+                )
+            )
+        ],
+        names=["test_system"],
+        topics=DataFrame(
+            [
+                {"qid": "q1"},
+                {"qid": "q2"},
+            ]
+        ),
+        qrels=DataFrame(
+            [
+                {"qid": "q1", "docno": "doc1", "label": 1},
+                {"qid": "q1", "docno": "doc2", "label": 0},
+                {"qid": "q2", "docno": "doc1", "label": 1},
+                {"qid": "q2", "docno": "doc2", "label": 0},
+            ]
+        ),
+        axioms=[_DOC_ID],
+        axiom_names=["_DOC_ID"],
+        text_field="text",
+    )
+
+
+def test_axiomatic_experiment_preferences(
+    axiomatic_experiment: AxiomaticExperiment,
+) -> None:
+    if not is_pyterrier_installed():
+        skip("PyTerrier is not installed.")
+
+    verify(
+        axiomatic_experiment.preferences.to_csv(
+            path_or_buf=None, index=False, header=True
+        )
+    )
+
+
+def test_axiomatic_experiment_preference_distribution(
+    axiomatic_experiment: AxiomaticExperiment,
+) -> None:
+    if not is_pyterrier_installed():
+        skip("PyTerrier is not installed.")
+
+    verify(
+        axiomatic_experiment.preference_distribution.to_csv(
+            path_or_buf=None, index=False, header=True
+        )
+    )
+
+
+def test_axiomatic_experiment_preference_consistency(
+    axiomatic_experiment: AxiomaticExperiment,
+) -> None:
+    if not is_pyterrier_installed():
+        skip("PyTerrier is not installed.")
+
+    verify(
+        axiomatic_experiment.preference_consistency.to_csv(
+            path_or_buf=None, index=False, header=True
+        )
+    )
+
+
+def test_axiomatic_experiment_inconsistent_pairs(
+    axiomatic_experiment: AxiomaticExperiment,
+) -> None:
+    if not is_pyterrier_installed():
+        skip("PyTerrier is not installed.")
+
+    verify(
+        axiomatic_experiment.inconsistent_pairs.to_csv(
+            path_or_buf=None, index=False, header=True
+        )
+    )
