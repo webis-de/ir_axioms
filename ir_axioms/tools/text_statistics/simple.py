2393374768366f33fc696da3fda3f12b1b54b05d
Jan Heinrich Merker <heinrich.merker@uni-jena.de>
2025-08-18 06:46:26 +0200

    Refactor text statistics to return pre-tokenized term counts

--- ir_axioms/tools/text_statistics/simple.py
+++ ir_axioms/tools/text_statistics/simple.py
@@ -4,6 +4,7 @@ from typing import Mapping, TypeVar
 
 from injector import inject
 
+from ir_axioms.model.utils import TokenizedString
 from ir_axioms.tools.text_statistics.base import TextStatistics
 from ir_axioms.tools.contents.base import TextContents
 from ir_axioms.tools.tokenizer.base import TermTokenizer
@@ -19,7 +20,8 @@ class SimpleTextStatistics(TextStatistics[T]):
     term_tokenizer: TermTokenizer
 
     def term_counts(self, document: T) -> Mapping[str, int]:
-        terms = self.term_tokenizer.terms(
-            text=self.text_contents.contents(input=document),
-        )
+        text = self.text_contents.contents(input=document)
+        if isinstance(text, TokenizedString):
+            return text.tokens
+        terms = self.term_tokenizer.terms(text)
         return Counter(terms)
